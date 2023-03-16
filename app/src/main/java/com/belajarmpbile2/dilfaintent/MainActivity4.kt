923a45dd22bb2e3374f826169a66819cff7482c9
pitsmail <farrazdavid21@gmail.com>
2023-03-16 15:40:15 +0700

    Nambah main activity baru untuk intent dengan data

--- app/src/main/java/com/belajarmpbile2/dilfaintent/MainActivity4.kt
+++ app/src/main/java/com/belajarmpbile2/dilfaintent/MainActivity4.kt
@@ -5,18 +5,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity4 : AppCompatActivity(), View.OnClickListener {
     companion object{
         const val EXTRA_TEXT = "extra_text"
     }
     private lateinit var buttono1 : Button
+    private lateinit var dilfa : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main4)
 
         buttono1 = findViewById(R.id.buttono1)
         buttono1.setOnClickListener(this)
+
+        val tvReceived = intent.getStringExtra(EXTRA_TEXT)
+        val text = "text yg dikerjain : $tvReceived"
+
+        dilfa.text = text
+
     }
     override fun onClick(v: View){
         when(v.id){
